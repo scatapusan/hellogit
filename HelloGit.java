943760efea7fdc5515a6c1dceb066e9fbb82de1f
Marco Dimaculangan <dimaculangan.marco@gmail.com>
2019-10-19 16:17:36 +0800

    First Timer

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,5 @@
 class HelloGit {
   public static void main(String[] args) {
-    System.out.println("Hello from Sam!");
+    System.out.println("Thanks for the learnings in Java Programming I from Marco Dimax!");
   }
 }
